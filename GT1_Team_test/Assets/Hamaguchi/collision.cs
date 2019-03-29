972288aaa8eb6afbdf6f2d71d8539ba96e01c28d
yoshiki hamaguchi <amakinton1996916@gmail.com>
2019-03-29 11:01:15 +0900

    commit

---
+++ GT1_Team_test/Assets/Hamaguchi/collision.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collision : MonoBehaviour
+{
+    int dustCounter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dustCounter = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "dust")
+        {
+            Destroy(collision.gameObject);
+            dustCounter++;
+            Debug.Log(dustCounter); // ログを表示する
+        }
+    }
+}
