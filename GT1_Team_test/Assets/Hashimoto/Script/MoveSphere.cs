2cf67b2434992e0ed0c96079a1c251c69abde720
HashimotoTomotake <tomo_10817@yahoo.co.jp>
2019-04-01 10:07:58 +0900

    ↓キー　ある部分が広がる ↑キー　ある部分が狭くなる 以上

---
+++ GT1_Team_test/Assets/Hashimoto/Script/MoveSphere.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveSphere : MonoBehaviour {
+
+    // サイズ
+    private float scale = 0.0f;
+    // 動くスピード
+    private const float SPEED = 0.05f;
+    // サイズの範囲
+    private const float MIU = 0.0f;     // 最小値
+    private const float MAX = 10.0f;    // 球の大きさ
+
+    void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        // キー操作でサイズを変える
+        if (!((Input.GetKey(KeyCode.UpArrow)) && (Input.GetKey(KeyCode.DownArrow))))
+        {   if (Input.GetKey(KeyCode.UpArrow))   scale += -SPEED;
+            if (Input.GetKey(KeyCode.DownArrow)) scale += +SPEED;
+        }
+        // 範囲の宣言
+        if (scale < MIU) scale = MIU;
+        if (scale > MAX) scale = MAX;
+
+        Debug.Log(scale);
+
+        //  サイズを変える
+        this.transform.localScale = new Vector3(scale,scale,scale);
+    }
+}
---
+++ GT1_Team_test/Assets/Hashimoto/Shader/BaseColor (1).shader
@@ -0,0 +1,14 @@
+﻿Shader "Custom/BaseColor (1)" {
+	Properties{
+		_Color("Color", Color) = (1,1,1,1)
+	}
+
+		SubShader{
+		Tags{ "Queue" = "Geometry-1" }
+
+		Pass{
+		Zwrite On
+		ColorMask 0
+	}
+	}
+}
