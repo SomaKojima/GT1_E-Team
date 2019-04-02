ef5ba2b55c0d14079dd39f83eac2faa24942c72c
TSITCL\s172235 <wadariku.19990129@gmail.com>
2019-04-02 15:46:29 +0900

    惑星が明るくなった時リザルトシーンへの移動 衝突時リザルトシーンへの移動

---
+++ GT1_Team_test/Assets/Wada/Assets/CameraControll.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControll : MonoBehaviour
+{
+    private GameObject mainCam; // メインカメラ
+    private GameObject subCam;  // サブカメラ
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // メインカメラとサブカメラを取得
+        mainCam = GameObject.Find("MainCamera");
+        subCam = GameObject.Find("SubCamera");
+
+        // サブカメラを非アクティブにする
+        subCam.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // スペースキーが押されたらサブカメラをアクティブにする
+        if(Input.GetKey("z"))
+        {
+            // サブカメラをアクティブに設定
+            mainCam.SetActive(false);
+            subCam.SetActive(true);
+        }
+        else
+        {
+            // メインカメラをアクティブに設定
+            mainCam.SetActive(true);
+            subCam.SetActive(false);
+        }
+    }
+}
---
+++ GT1_Team_test/Assets/Wada/Assets/ChaseCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaseCamera : MonoBehaviour
+{
+    public GameObject traget;
+    private Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - traget.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = traget.transform.position + offset;
+    }
+}
