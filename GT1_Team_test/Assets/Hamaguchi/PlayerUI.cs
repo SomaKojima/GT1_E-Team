794c4a6f523dbf97e944e88ce2279b6ad676daa4
yoshiki hamaguchi <amakinton1996916@gmail.com>
2019-04-06 05:40:55 +0900

    UI関連

--- GT1_Team_test/Assets/Hamaguchi/PlayerUI.cs
+++ GT1_Team_test/Assets/Hamaguchi/PlayerUI.cs
@@ -18,13 +18,13 @@ public class PlayerUI : MonoBehaviour
 
     private int count = 0;
     private bool flagA = true;
-    private bool menuFlag = false;
+    private bool menuFlag = true;
     private int menuState = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        menuState = 0;
     }
 
     // Update is called once per frame
@@ -40,27 +40,40 @@ public class PlayerUI : MonoBehaviour
 
         if ((player.GetComponent<collision>().GetTalkFlag()) && (flagA))
         {
-            menuState = 2;
-            if (count < 15)
+            if (count == 0)
             {
-                mission.transform.Translate(20.0f, 0, 0);
+                if(!menuFlag)
+                {
+                    mission.transform.Translate(300.0f, 0, 0);
+                }
+                menuState = 2;
                 menuFlag = true;
                 flagA = false;
-                count = 0;
-                menuState = 1;
             }
         }
 
-        if(menuState != 0)
+        if (menuState != 0)
         {
             count++;
-            if(count<16)
+            if (count < 16)
             {
-
+                if (menuFlag)
+                {
+                    mission.transform.Translate(20.0f, 0, 0);
+                }
+                else
+                {
+                    mission.transform.Translate(-20.0f, 0, 0);
+                }
+            }
+            else
+            {
+                count = 0;
+                menuState = 0;
             }
         }
 
-        if (Input.GetKeyDown(KeyCode.X)&&(flagA))
+        if (Input.GetKeyDown(KeyCode.X))
         {
             if (menuState == 0)
             {
