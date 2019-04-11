6cdb9e943f2b6329eac4bb0b2cea2f56195bacd1
yoshiki hamaguchi <amakinton1996916@gmail.com>
2019-04-11 16:03:11 +0900

    コミット

--- GT1_Team_test/Assets/Hamaguchi/script/PaleLight.cs
+++ GT1_Team_test/Assets/Hamaguchi/script/PaleLight.cs
@@ -5,15 +5,24 @@ using UnityEngine;
 public class PaleLight : MonoBehaviour
 {
     private Vector3 aPos;
+    [SerializeField]
+    private GameObject lightPrefab;
     // Start is called before the first frame update
     void Start()
     {
-        
+        aPos = this.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float dis = Vector3.Distance(this.transform.position, aPos);
+        if(dis>10)
+        {
+            GameObject obj;
+            Quaternion q = Quaternion.LookRotation(Vector3.zero- aPos);
+            obj = Instantiate(lightPrefab, this.transform.position, q) as GameObject;
+            aPos = this.transform.position;
+        }
     }
 }
