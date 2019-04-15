3d166882d35dd821fe91838b374158f391ea167a
kojima1016 <kojisou1016@gmail.com>
2019-04-15 12:01:01 +0900

    通常カメラ、話す用のカメラのクラス化

---
+++ GT1_Team_test/Assets/Kojima/Script/Camera/CameraController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public enum CameraMode
+    {
+        Nomal,
+        Talk
+    }
+    /// <summary>
+    /// カメラのターゲット    
+    /// </summary>
+    public GameObject player;
+    // ターゲットが吸い寄せられている惑星
+    public GameObject planet;
+
+    // 話しかける時の位置
+    public Transform talkPosition;
+    // 話しかける相手
+    public Transform talkTarget;
+
+    // カメラのモード
+    public CameraMode cameraMode = CameraMode.Nomal;
+    // カメラとターゲットの最大距離
+    public float max_distance = 5;
+    // カメラのローカル座標（ターゲットの座標）Y軸のオフセット
+    public float offset_y = 10;
+    // カメラの上方向
+    Vector3 up = Vector3.forward;
+
+    // ノーマルカメラ
+    NormalCamera normalCamera = new NormalCamera();
+    // 話す用のカメラ
+    TalkCamera talkCamera = new TalkCamera();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 通常カメラの設定
+        normalCamera.Start(this.gameObject, player, planet);
+        normalCamera.max_distance = max_distance;
+        normalCamera.offset_y = offset_y;
+
+        // 話す用のカメラの設定
+        talkCamera.Start(this.gameObject, talkPosition, talkTarget, player);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+
+        switch (cameraMode)
+        {
+            case CameraMode.Nomal:
+                normalCamera.Update();
+                break;
+            case CameraMode.Talk:
+                talkCamera.Update();
+                break;
+        }
+    }
+
+    /// <summary>
+    /// 話す用カメラに変える
+    /// </summary>
+    /// <param name="talkChara">話す相手</param>
+    /// <returns>変えられたかどうか</returns>
+    public bool ChangeTalkMode(Transform talkChara, Transform cameraPos)
+    {
+        if (!talkChara) return false;
+        cameraMode = CameraMode.Talk;
+        talkTarget = talkChara;
+        talkPosition = cameraPos;
+        return true;
+    }
+}
---
+++ GT1_Team_test/Assets/Kojima/Script/Camera/NormalCamera.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NormalCamera
+{
+    public GameObject camera;
+    // カメラのターゲット    
+    public GameObject player;
+    // ターゲットが吸い寄せられている惑星
+    public GameObject planet;
+
+    // カメラとターゲットの最大距離
+    public float max_distance = 5;
+    // カメラのローカル座標（ターゲットの座標）Y軸のオフセット
+    public float offset_y = 10;
+    // カメラの上方向
+    Vector3 up = Vector3.forward;
+
+    // Start is called before the first frame update
+    public void Start(GameObject _camera, GameObject _player, GameObject _planet)
+    {
+        camera = _camera;
+        player = _player;
+        planet = _planet;
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        Debug.Log("camera");
+        NormalCameraMove();
+        NormalCameraRotation();
+    }
+
+    /// <summary>
+    /// 通常カメラの移動
+    /// </summary>
+    void NormalCameraMove()
+    {
+        // 惑星からプレイヤーまでの回転(ワールド座標に戻す用)
+        Quaternion q = Quaternion.FromToRotation(Vector3.up, (player.transform.position - planet.transform.position).normalized);
+        // 逆回転(ローカル座標に戻す用)
+        Quaternion inv = Quaternion.Inverse(q);
+        // カメラのローカル座標を計算
+        Vector3 localPos = camera.transform.position - player.transform.position;
+        localPos = inv * localPos;
+        // Y軸は固定（高さは固定）
+        localPos.y = offset_y;
+        // ベクトルを計算
+        Vector3 vec = -localPos;
+        // Y軸の動きをなくす
+        vec.y = 0;
+        float length = vec.magnitude;
+        Vector3 vec_normal = vec.normalized;
+
+        Debug.Log(length);
+        Vector3 move_vec = Vector3.zero;
+
+        // カメラの距離が遠い場合
+        if (max_distance < length)
+        {
+            // 範囲の距離までの差分を計算
+            float move_length = length - max_distance;
+            // ターゲットに向けて移動量
+            move_vec = vec_normal * move_length;
+        }
+
+        // ローカル座標をワールド座標に戻す
+        Vector3 worldPos = player.transform.position + (q * localPos);
+
+        Vector3 pos = worldPos + (q * move_vec);
+        camera.transform.position = Vector3.Slerp(camera.transform.position, pos, 1.0f);
+    }
+
+    /// <summary>
+    /// 通常カメラの回転
+    /// </summary>
+    void NormalCameraRotation()
+    {
+        // ターゲットまでのベクトル
+        Vector3 vec = player.transform.position - camera.transform.position;
+        // 惑星までのベクトル
+        Vector3 vec2 = planet.transform.position - camera.transform.position;
+        // ターゲットまでのベクトルとカメラの上方向で軸を作成
+        Vector3 axis = Vector3.Cross(vec, camera.transform.up);
+        // 惑星までのベクトルとaxisでカメラの上方向を作成
+        Vector3 up = Vector3.Cross(axis, vec2);
+
+        camera.transform.LookAt(player.transform.position, up);
+    }
+
+}
