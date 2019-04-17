8798c3594e17b84c9f8a42110451ffaf4eef6256
kojima1016 <kojisou1016@gmail.com>
2019-04-17 09:28:42 +0900

    カメラとプレイヤーの間を透過するが途中

---
+++ GT1_Team_test/Assets/Kojima/Script/Camera/CameraRayTransparent.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRayTransparent : MonoBehaviour
+{
+    public GameObject camera;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(camera.transform.position, player.transform.position);
+        Ray ray = new Ray(camera.transform.position, camera.transform.forward);
+
+        RaycastHit[] hits = Physics.RaycastAll(ray, distance);
+
+        foreach (RaycastHit hit in hits)
+        {
+            Renderer render = hit.collider.gameObject.GetComponent<Renderer>();
+            if (render)
+            {
+                Debug.Log(hit.collider.name);
+                hit.collider.gameObject.GetComponent<Renderer>().material.color = new Color(render.material.color.r, render.material.color.g, render.material.color.b, 0.0f);
+            }
+        }
+    }
+}
