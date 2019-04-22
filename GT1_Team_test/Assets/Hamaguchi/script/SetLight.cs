daec90cb78919b400b8f4b0a71827dde611aedbc
yoshiki hamaguchi <amakinton1996916@gmail.com>
2019-04-22 10:27:40 +0900

    commit

---
+++ GT1_Team_test/Assets/Hamaguchi/script/SetLight.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetLight : MonoBehaviour
+{
+    private Vector3 v1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
