cf80ea7f5701e45883993f127af7f8fa978b7d6d
MiuHimi <miucchi207@gmail.com>
2019-05-28 18:45:56 +0900

    commit

---
+++ GT1_Team_test/Assets/Himi/Source/ChengeSceneFade.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ChengeSceneFade : MonoBehaviour
+{
+    private Image m_imageRenderer;       // スプライトレンダラー
+    public string sceneName;
+
+    private  StartToPlay startToPlayCS;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_imageRenderer = this.gameObject.GetComponent<Image>();
+        startToPlayCS = GameObject.Find("start_ui").GetComponent<StartToPlay>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var color = m_imageRenderer.color;
+
+        // シーン遷移が起こったら
+        if (startToPlayCS.isChengeSceneFlag == true)
+        {
+            // フェード
+            color.a += 0.01f;
+            m_imageRenderer.color = color;
+        }
+
+        if(color.a >= 2.0)
+        {
+            color.a = 2.0f;
+
+            SceneManager.LoadScene(sceneName);
+        }
+
+        Debug.Log(color.a);
+    }
+}
