d85e7b49061a497e8d0eb4cc9bdac1138b1f9231
Hashimoto__Tomotake <tomo_10817@yahoo.co.jp>
2019-05-29 11:06:02 +0900

    stage1,2  BGM追加

---
+++ GT1_Team_test/Assets/Hasimoto/BGMSound/Script/BGMSound.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// BGMを鳴らす
+/// </summary>
+public class BGMSound : MonoBehaviour
+{
+    // BGM名
+    public string BGM_Name;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 音を鳴らす
+        SoundManager.Instance.PlayBgm(BGM_Name);
+
+    }
+
+}
